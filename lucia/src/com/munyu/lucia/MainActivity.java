89c4bd5bc5b985c3c7abc7bb35c4b4aa80a20737
munyu720 <BYG01046@gmail.com>
2013-01-28 21:15:10 +0900

    シークバーと明滅秒数の表示用のテキストビューを実装

--- lucia/src/com/munyu/lucia/MainActivity.java
+++ lucia/src/com/munyu/lucia/MainActivity.java
@@ -2,9 +2,13 @@ package com.munyu.lucia;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -14,10 +18,45 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//Tweenアニメーションの適用
-		ImageView img = (ImageView) findViewById(R.id.imageView3);
-		Animation animation= AnimationUtils.loadAnimation(this,R.anim.lightanim);
+		// Tweenアニメーションの適用
+		// 初期状態のアニメーションを表示
+		final ImageView img = (ImageView) findViewById(R.id.imageView3);
+		final Animation animation= AnimationUtils.loadAnimation(this,R.anim.lightanim);
 		img.startAnimation(animation);
-
+		
+		final TextView textView = (TextView) findViewById(R.id.textView2);
+		
+		// シークバーの設定
+		// 変更後に明滅間隔を変更
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar1);
+        seekBar.setMax(100);
+        seekBar.setProgress(50);
+        // テキストビューのテキストを設定します
+        textView.setText(String.valueOf((seekBar.getProgress()*30.00)/1500)+"秒に1回点滅");
+        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            // トラッキング開始時に呼び出されます
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.v("onStartTrackingTouch()",
+                    String.valueOf(seekBar.getProgress()));
+            }
+            // トラッキング中に呼び出されます
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+                Log.v("onProgressChanged()",
+                    String.valueOf(progress) + ", " + String.valueOf(fromTouch));
+            }
+            // トラッキング終了時に呼び出されます
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.v("onStopTrackingTouch()",
+                    String.valueOf(seekBar.getProgress()));
+                animation.setDuration((long) ((seekBar.getProgress()*30)/1.5));
+             // テキストビューのテキストを設定します
+                textView.setText(String.valueOf((seekBar.getProgress()*30.00)/1500)+"秒に1回点滅");
+                img.startAnimation(animation);
+            }
+            
+        });
 	}
 }
