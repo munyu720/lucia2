f69a9f0766909400484998c7202eaff25d0dd741
munyu720 <BYG01046@gmail.com>
2013-01-30 19:54:42 +0900

    明滅表示の基幹式を仮設定 構造を整理

--- lucia/gen/com/munyu/lucia/R.java
+++ lucia/gen/com/munyu/lucia/R.java
@@ -13,7 +13,11 @@ public final class R {
     }
     public static final class array {
         public static final int list_entries=0x7f060000;
+        public static final int list_entries2=0x7f060002;
+        public static final int list_entries3=0x7f060004;
         public static final int list_entryvalues=0x7f060001;
+        public static final int list_entryvalues2=0x7f060003;
+        public static final int list_entryvalues3=0x7f060005;
     }
     public static final class attr {
     }
@@ -43,7 +47,11 @@ public final class R {
         public static final int credit=0x7f070001;
         public static final int list_summary=0x7f070004;
         public static final int list_title=0x7f070003;
+        public static final int meimetu_sec_summary=0x7f070006;
+        public static final int meimetu_sec_title=0x7f070005;
         public static final int pref_title=0x7f070002;
+        public static final int seek_sec_summary=0x7f070008;
+        public static final int seek_sec_title=0x7f070007;
     }
     public static final class style {
         /** 
--- lucia/src/com/munyu/lucia/MainActivity.java
+++ lucia/src/com/munyu/lucia/MainActivity.java
@@ -16,61 +16,41 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity 
+		implements OnSeekBarChangeListener{
 
+	  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+        
+		//設定画面の初期設定
 		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 		
-		// テキストビューの初期設定
-		final TextView textView1 = (TextView) findViewById(R.id.textView1);
+		// ホタルデータの初期設定
+		TextView textView1 = (TextView) findViewById(R.id.textView1);
         textView1.setText(sharedPref.getString("list_key", ""));
-		
+        Integer meimetu_sec = Integer.valueOf(sharedPref.getString("meimetu_sec", "2000"));
+        Integer seek_sec = Integer.valueOf(sharedPref.getString("seek_sec", "1000"));
+        
 		// Tweenアニメーションの適用
 		// 初期状態のアニメーションを表示
-		final ImageView img = (ImageView) findViewById(R.id.imageView3);
-		final Animation animation= AnimationUtils.loadAnimation(this,R.anim.lightanim);
+		ImageView img = (ImageView) findViewById(R.id.imageView3);
+		Animation animation= AnimationUtils.loadAnimation(this,R.anim.lightanim);
 		img.startAnimation(animation);
 		
-		// テキストビューの初期設定
-		final TextView textView2 = (TextView) findViewById(R.id.textView2);
-		
-		// シークバーの設定
-		// 変更後に明滅間隔を変更
+		// シークバーの初期設定
         SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar1);
         seekBar.setMax(100);
         seekBar.setProgress(50);
-        // テキストビューのテキストを設定します
-        textView2.setText(String.valueOf((seekBar.getProgress()*30.00)/1500)+"秒に1回点滅");
-        seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-           
-        	// トラッキング開始時に呼び出されます
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                Log.v("onStartTrackingTouch()",
-                    String.valueOf(seekBar.getProgress()));
-            }
-            // トラッキング中に呼び出されます
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
-                Log.v("onProgressChanged()",
-                    String.valueOf(progress) + ", " + String.valueOf(fromTouch));
-            }
-            // トラッキング終了時に呼び出されます
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                Log.v("onStopTrackingTouch()",
-                    String.valueOf(seekBar.getProgress()));
-                animation.setDuration((long) ((seekBar.getProgress()*30)/1.5));
-             // テキストビューのテキストを設定します
-                textView2.setText(String.valueOf((seekBar.getProgress()*30.00)/1500)+"秒に1回点滅");
-                img.startAnimation(animation);
-            }
-            
-        });
+		TextView textView2 = (TextView) findViewById(R.id.textView2);
+		if (seekBar.getProgress() == 50) {
+	        textView2.setText(String.valueOf((meimetu_sec-(seek_sec))/1000.00) + "秒に1回点滅");
+		} else {
+	        textView2.setText(String.valueOf((meimetu_sec-(seek_sec/(50-seekBar.getProgress())))/1000.00) + "秒に1回点滅");
+		}
+        seekBar.setOnSeekBarChangeListener(this);
 	}
 	
 	@Override  
@@ -78,11 +58,45 @@ public class MainActivity extends Activity {
 	    super.onResume();  
 		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 		
-		// テキストビューの初期設定
-		final TextView textView1 = (TextView) findViewById(R.id.textView1);
+		// ホタル名表示に設定の値を更新
+		TextView textView1 = (TextView) findViewById(R.id.textView1);
         textView1.setText(sharedPref.getString("list_key", ""));
 	}  
+		
+	//シークバーの処理関連
 	
+	// シークバートラッキング開始時
+	public void onStartTrackingTouch(SeekBar seekBar) {
+        Log.v("onStartTrackingTouch()",
+            String.valueOf(seekBar.getProgress()));
+    }
+    // シークバートラッキング中
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+        Log.v("onProgressChanged()",
+            String.valueOf(progress) + ", " + String.valueOf(fromTouch));
+    }
+    // シークバートラッキング終了時
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        Log.v("onStopTrackingTouch()",
+            String.valueOf(seekBar.getProgress()));
+		ImageView img = (ImageView) findViewById(R.id.imageView3);
+		Animation animation= AnimationUtils.loadAnimation(this,R.anim.lightanim);
+		img.startAnimation(animation);
+		TextView textView2 = (TextView) findViewById(R.id.textView2);
+		SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        Integer meimetu_sec = Integer.valueOf(sharedPref.getString("meimetu_sec", "2000"));
+        Integer seek_sec = Integer.valueOf(sharedPref.getString("seek_sec", "1000"));
+		animation.setDuration((long) ((seekBar.getProgress()*30)/1.5));
+		if (seekBar.getProgress() == 50) {
+	        textView2.setText(String.valueOf((meimetu_sec-(seek_sec))/1000.00) + "秒に1回点滅");
+		} else {
+	        textView2.setText(String.valueOf((meimetu_sec-(seek_sec/(50-seekBar.getProgress())))/1000.00) + "秒に1回点滅");
+		}
+        img.startAnimation(animation);
+    }
+    
+    //設定関係
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
@@ -100,5 +114,5 @@ public class MainActivity extends Activity {
 		}
 		return false;
 	}
-	
+
 }
